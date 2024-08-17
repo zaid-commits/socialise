cbf7fb1f53222025916937e2f2100816613a4d20
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-08-17 07:59:42 +0530

    little good

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ new Vue({
         cardLayout: 'classic',
         showLanguage: true,
         useGradient: false,
+        sharableImageUrl: '',
         languageColors: {
             JavaScript: '#f1e05a',
             Python: '#3572A5',
@@ -36,14 +37,24 @@ new Vue({
         async downloadImage() {
             const element = document.querySelector('.repo-card');
             const canvas = await html2canvas(element, {
-                backgroundColor: this.cardTheme === 'light' ? '#ffffff' : 
+                backgroundColor: this.cardTheme === 'light' ? '#ffffff' :
                                  this.cardTheme === 'dark' ? '#24292e' : '#0d1117',
                 scale: 2 // Increase resolution
             });
+            const dataURL = canvas.toDataURL('image/png');
+            // Create a link to download the image
             const link = document.createElement('a');
             link.download = 'repo-card.png';
-            link.href = canvas.toDataURL('image/png');
+            link.href = dataURL;
             link.click();
+            // Set the sharable image URL
+            this.sharableImageUrl = dataURL;
+        },
+        copyToClipboard() {
+            const urlInput = document.querySelector('.url-container input');
+            urlInput.select();
+            document.execCommand('copy');
+            alert('URL copied to clipboard!');
         }
     }
 });
